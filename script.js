61b8fcba731ce9171b2baa515ea868a6fc349e7a
Filip Bukowiec <filip.bukowiec@interia.pl>
2022-07-24 19:46:05 +0200

    init

---
+++ script.js
@@ -0,0 +1,19 @@
+const button1 = document.getElementById("Button1");
+button1.addEventListener("click", () => {
+  button1.innerText = "Filip Bukowiec";
+});
+
+const button2 = document.querySelector(".btn2");
+button2.addEventListener("click", () => {
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("animation");
+  button2.after(newDiv);
+  button2.innerText = "Mój loader";
+}, { once: true } );
+
+const button3 = document.getElementById("Button3");
+button3.addEventListener("click", () => {
+  window.alert(
+    "Swoją przygodę z Frontendem rozpocząłem w 2022r. Mam nadzieję, że jestem na dobrej drodze. Pozdrawiam, Filip Bukowiec"
+  );
+});
